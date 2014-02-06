dc8a83e64c8f45f9f2f7d4ce4fa113db124d2c5b
megazear7 <alexlockhart7@gmail.com>
2014-02-06 13:04:53 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocationSaver::Application.config.secret_key_base = 'ae87ce54a001ee3308a663d749333b8f80e716acab3f5d530a7718735f3b3a932def5f8bb4fd1b3e21703741eb60dc93f6f7294d7409266c1fbbb86eff0685d5'
