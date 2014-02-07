7d772db4c0c150f23a429e9bd701562bffb4390e
megazear7 <alexlockhart7@gmail.com>
2014-02-07 17:03:24 -0500

    added two new tests

--- spec/requests/make_places_spec.rb
+++ spec/requests/make_places_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 require 'factory_girl'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 describe "MakePlaces", :type => :feature do
-  it "create place for user when user complete place creation form" do
-    user = FactoryGirl.create(:user)
+
+  it "logs in user and then follows links to create a place for user when user complete place creation form" do
+    user = create(:user)
     visit root_path
     click_link "Log in"
     fill_in "Email", :with => user.email
@@ -15,4 +18,21 @@ describe "MakePlaces", :type => :feature do
     fill_in "Icon url", :with => "www.example_icon_url.com"
     click_button "Create Place"
   end
+
+  it "creates a new place for the user" do
+    user = create(:user)
+    login_as(user, :scope => :user)
+    visit new_place_path
+    fill_in "Title", :with => "Test Title"
+    fill_in "Icon url", :with => "www.example_icon_url.com"
+    click_button "Create Place"
+  end
+
+  it "goes to the private page and adds place from the list" do
+    user = create(:user)
+    login_as(user, :scope => :user)
+    visit private_path
+    first(:link, "Add to My Places").click
+  end
+
 end
